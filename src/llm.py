975994a62204764224ed54f74461508cfb0209b6
Jasleen Kaur <kaurjasleen6104@gmail.com>
2026-07-13 15:18:50 +0530

    Add LLM functionality with ChatOllama integration in the src/llm.py module

---
+++ src/llm.py
@@ -0,0 +1,10 @@
+from src.config import LLM_MODEL
+
+from langchain_ollama import ChatOllama
+
+def get_llm():
+    llm = ChatOllama(
+        model = LLM_MODEL,
+        temperature = 0
+    )
+    return llm
